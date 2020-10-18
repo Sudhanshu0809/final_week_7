c3218bf0ab77c28232f998b8769470f0349d563d
VasileiosXynogalos <70195652+VasileiosXynogalos@users.noreply.github.com>
2020-10-18 08:01:05 +0300

    Add files via upload

---
+++ server.R
@@ -0,0 +1,45 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+source("predictInput.R")
+
+library(DT)
+library(shiny)
+library(shinydashboard)
+library(wordcloud)
+library(RColorBrewer)
+
+shinyServer(
+    function(input, output) {
+        prediction <-  reactive( {
+            inputText <- input$text
+            predictFunction(inputText)
+        })
+        
+        output$table <- renderDT({
+            if (is.null(prediction))
+                return(NULL)
+            datatable(prediction(), options = list(lengthMenu = c(5, 10, 25)))
+        }) 
+        
+        output$topPrediction <- renderValueBox({
+            
+            valueBox(
+                paste0(prediction()$lastGram[1]), "Next word", icon = icon("list"),
+                color = "olive")
+        })
+        
+        wordcloud_rep <- repeatable(wordcloud)
+        output$plot <- renderPlot(
+            wordcloud_rep(
+                prediction()$lastGram,
+                prediction()$p,
+                use.r.layout = FALSE, random.order = FALSE,
+                colors = brewer.pal(6, 'Dark2'), scale=c(4, 1), max.words = 50))
+    })
